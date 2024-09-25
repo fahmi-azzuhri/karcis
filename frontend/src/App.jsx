41c119c3ff48d7e26d3bfd56f4d186fb67e36c98
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-09-25 21:26:34 +0700

    refactor navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Navbar, IconButton, Collapse } from "@material-tailwind/react";
+import { IconButton, Collapse } from "@material-tailwind/react";
 import logo_karcis from "/img/logo/logo_karcis.png";
+import AppRoutes from "./routes";
 function App() {
   const [openNav, setOpenNav] = useState(false);
   const textNavbar = [
@@ -20,58 +21,68 @@ function App() {
     </ul>
   );
   return (
-    <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-white rounded-lg">
-      <div className="flex items-center justify-between">
-        <img src={logo_karcis} alt="" />
-        <div className="flex items-center gap-4">
-          <div className="mr-4 hidden lg:block">{navList}</div>
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 lg:hidden"
-            ripple={false}
-            onClick={() => setOpenNav(!openNav)}
-          >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </IconButton>
+    <>
+      <nav className="sticky top-0 z-10 max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-white rounded-lg border border-gray-300">
+        <div className="flex items-center justify-between">
+          <img
+            src={logo_karcis}
+            className=" sm:h-8 md:h-12 lg:h-16 w-auto"
+            alt=""
+          />
+          <div className="flex items-center gap-4">
+            <div className="mr-4 hidden lg:block">{navList}</div>
+            <IconButton
+              variant="text"
+              className="ml-auto h-6 w-6 lg:hidden"
+              ripple={false}
+              onClick={() => setOpenNav(!openNav)}
+            >
+              {openNav ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </IconButton>
+          </div>
+          <button className="rounded-lg bg-blue-500 px-5 py-2 hover:bg-blue-600">
+            Login
+          </button>
         </div>
-        <button className="rounded-lg bg-blue-500 px-5 py-2 hover:bg-blue-600">
-          Login
-        </button>
-      </div>
 
-      <Collapse open={openNav} className="lg:hidden">
-        {openNav && <div className="block lg:hidden">{navList}</div>}
-      </Collapse>
-    </Navbar>
+        <Collapse open={openNav} className="lg:hidden">
+          {openNav && <div className="block lg:hidden">{navList}</div>}
+        </Collapse>
+      </nav>
+
+      <div>
+        <AppRoutes />
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/routes/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppRoutes() {
+  return <div>index</div>;
+}
+
+export default AppRoutes;
