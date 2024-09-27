c0a89cbe2655cd51c679bcedacbcf5532bf7a2d4
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-09-27 22:59:34 +0700

    atomic home component

--- frontend/src/views/home/index.jsx
+++ frontend/src/views/home/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import bg_home from "/img/bg/bg_home.png";
+import SearchBar from "../../components/home/SearchBar";
 function Home() {
   return (
     <div className="relative">
@@ -9,20 +10,7 @@ function Home() {
           Exclusive events, priceless moments
         </h1>
       </div>
-      <div className="absolute bottom-[-35px] left-1/2 transform -translate-x-1/2 w-3/4 bg-white p-4 rounded-lg shadow-lg flex items-center space-x-4">
-        <input
-          type="text"
-          placeholder="Search by events, name, location, and more"
-          className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <input
-          type="date"
-          className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <button className="bg-blueDefault text-white px-6 py-2 rounded-lg hover:bg-blueDarkest">
-          Search
-        </button>
-      </div>
+      <SearchBar />
     </div>
   );
 }
