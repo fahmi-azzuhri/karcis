5e8e6afe939b419fa8e8c18987e6cb018ba9a30e
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-09-29 23:58:04 +0700

    add carousel upcoming event

---
+++ frontend/src/components/event/upcomingEvent/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Carousels from "./carousel";
+
+function UpcomingEvent() {
+  return (
+    <div className="px-12">
+      <h3 className="text-2xl font-bold text-blackDefault">Upcoming Event</h3>
+      <Carousels />
+    </div>
+  );
+}
+
+export default UpcomingEvent;
--- frontend/src/views/home/index.jsx
+++ frontend/src/views/home/index.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import bg_home from "/img/bg/bg_home.png";
 import SearchBar from "../../components/home/SearchBar";
+import UpcomingEvent from "../../components/event/upcomingEvent";
 function Home() {
   return (
-    <div className="relative">
-      <img src={bg_home} alt="Background" className="w-full h-auto" />
-      <div className="absolute top-1/4 w-full text-center">
-        <h1 className="text-4xl font-bold text-blueDefault">
-          Exclusive events, priceless moments
-        </h1>
+    <>
+      <div className="relative">
+        <img src={bg_home} alt="Background" className="w-full h-auto" />
+        <div className="absolute top-1/4 w-full text-center">
+          <h1 className="text-5xl font-bold text-blueDefault">
+            Exclusive events, priceless moments
+          </h1>
+        </div>
+        <SearchBar />
       </div>
-      <SearchBar />
-    </div>
+      <div className="p-7 mt-10 bg-white">
+        <UpcomingEvent />
+      </div>
+    </>
   );
 }
 export default Home;
