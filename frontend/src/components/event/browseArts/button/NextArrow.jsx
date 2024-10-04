a27d1770933a0690dc0697f3d7b67338b303122b
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-10-04 22:18:42 +0700

    add browse arts carousel

---
+++ frontend/src/components/event/browseArts/button/NextArrow.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function NextArrow(props) {
+  const { IoIosArrowForward, style, onClick } = props;
+  return (
+    <div
+      className={`absolute right-0 top-1/2 transform -translate-y-1/2 bg-transparent text-gray-800 border border-gray-500 rounded-full p-2 z-10 cursor-pointer`}
+      style={{ ...style }}
+      onClick={onClick}
+    >
+      <IoIosArrowForward />
+    </div>
+  );
+}
+
+export default NextArrow;
--- frontend/src/components/event/index.jsx
+++ frontend/src/components/event/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UpcomingEvent from "./upcomingEvent";
 import TopSelling from "./topSelling";
+import BrowseArts from "./browseArts";
 
 function Events() {
   return (
@@ -12,6 +13,10 @@ function Events() {
       <div className="px-12">
         <h3 className="text-2xl font-bold text-blackDefault">Top Selling</h3>
         <TopSelling />
+      </div>
+      <div className="px-12">
+        <h3 className="text-2xl font-bold text-blackDefault">Browse Arts</h3>
+        <BrowseArts />
       </div>
     </>
   );
