1d94089bdeb5200de163eba43c4d2eefd77656d2
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-10-07 02:41:10 +0700

    add footer

---
+++ frontend/src/views/footer/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function ViewFooter(props) {
+  const { about, partner, support, app, ios, android } = props;
+  return (
+    <footer className="bg-blueDefault text-white">
+      <div className="container mx-auto px-8 py-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8">
+          <h2 className="font-semibold text-xl">Karcis.com</h2>
+          <div className="flex flex-col">
+            <h3 className="font-semibold mb-4">About</h3>
+            {about.map((item) => (
+              <p className="mb-1" key={item}>
+                {item}
+              </p>
+            ))}
+          </div>
+          <div className="flex flex-col">
+            <h3 className="font-semibold mb-4">Partner with us</h3>
+            {partner.map((item) => (
+              <p className="mb-1" key={item}>
+                {item}
+              </p>
+            ))}
+          </div>
+          <div className="flex flex-col">
+            <h3 className="font-semibold mb-4">Support</h3>
+            {support.map((item) => (
+              <p className="mb-1" key={item}>
+                {item}
+              </p>
+            ))}
+          </div>
+          <div className="flex flex-col">
+            <h3 className="font-semibold mb-4">App</h3>
+            {app.map((item) => (
+              <p className="mb-1" key={item}>
+                {item}
+              </p>
+            ))}
+            <img className="w-32 mb-1" src={ios} alt="" />
+            <img className="w-32" src={android} alt="" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default ViewFooter;
--- frontend/src/views/home/index.jsx
+++ frontend/src/views/home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import bg_home from "/img/bg/bg_home.png";
 import SearchBar from "../../components/home/SearchBar";
 import Events from "../../components/event";
+import Footer from "../../components/footer";
 
 function Home() {
   return (
@@ -17,6 +18,9 @@ function Home() {
       </div>
       <div className="p-7 mt-10 bg-white">
         <Events />
+      </div>
+      <div>
+        <Footer />
       </div>
     </>
   );
