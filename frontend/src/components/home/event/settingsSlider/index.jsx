a09fee140d0a59c1a34d3a2433202eb083fba72f
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-10-14 11:55:19 +0700

    atomic settings slider

---
+++ frontend/src/components/home/event/settingsSlider/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import NextArrow from "../button/NextArrow";
+import PrevArrow from "../button/PrevArrow";
+function SettingsSlider() {
+  const settings = {
+    centerMode: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    swipe: true,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return settings;
+}
+
+export default SettingsSlider;
--- frontend/src/components/home/event/upcomingEvent/index.jsx
+++ frontend/src/components/home/event/upcomingEvent/index.jsx
@@ -2,52 +2,11 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import NextArrow from "../button/NextArrow";
-import PrevArrow from "../button/PrevArrow";
 import ViewUpcomingEvent from "../../../../views/home/event/upcomingEvent";
+import SettingsSlider from "../settingsSlider";
 
 function UpcomingEvent() {
-  const settings = {
-    centerMode: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    swipe: true,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+  const settings = SettingsSlider();
 
   const cards = [
     {
