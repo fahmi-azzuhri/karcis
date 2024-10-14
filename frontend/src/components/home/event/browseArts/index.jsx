5eb9fa1be3673ecbbe29033561e32855bb426aea
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-10-14 11:39:36 +0700

    fix button prev next

--- frontend/src/components/home/event/browseArts/index.jsx
+++ frontend/src/components/home/event/browseArts/index.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import NextArrow from "./button/NextArrow";
-import PrevArrow from "./button/PrevArrow";
+import NextArrow from "../button/NextArrow";
+import PrevArrow from "../button/PrevArrow";
 import ViewBrowseArts from "../../../../views/home/event/browseArts";
 
 function BrowseArts() {
@@ -14,8 +13,8 @@ function BrowseArts() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    nextArrow: <NextArrow IoIosArrowForward={IoIosArrowForward} />,
-    prevArrow: <PrevArrow IoIosArrowBack={IoIosArrowBack} />,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     swipe: true,
     initialSlide: 0,
     responsive: [
--- frontend/src/components/home/event/upcomingEvent/index.jsx
+++ frontend/src/components/home/event/upcomingEvent/index.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import NextArrow from "./button/NextArrow";
-import PrevArrow from "./button/PrevArrow";
+import NextArrow from "../button/NextArrow";
+import PrevArrow from "../button/PrevArrow";
 import ViewUpcomingEvent from "../../../../views/home/event/upcomingEvent";
 
 function UpcomingEvent() {
@@ -14,8 +13,8 @@ function UpcomingEvent() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    nextArrow: <NextArrow IoIosArrowForward={IoIosArrowForward} />,
-    prevArrow: <PrevArrow IoIosArrowBack={IoIosArrowBack} />,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     swipe: true,
     initialSlide: 0,
     responsive: [
