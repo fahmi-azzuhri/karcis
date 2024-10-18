54a55992fcb33ad53a2ea5330a0102622b3b6cb2
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-10-18 14:17:01 +0700

    fix method payment page

--- frontend/src/components/payment/paymentMethod/Ewallet.jsx
+++ frontend/src/components/payment/paymentMethod/Ewallet.jsx
@@ -1,52 +1,55 @@
-import React from "react";
-
 function Ewallet() {
   return (
     <div>
       <h3 className="text-lg font-semibold mb-2">Electronic Money</h3>
       <div className="space-y-2">
-        <div className="mb-4 border p-2 rounded rounded-sm flex flex-row items-center">
-          <input
-            type="radio"
-            id="gopay"
-            name="paymentMethod"
-            className="mr-2"
-          />
+        <div className="mb-4 border p-2 rounded rounded-sm flex justify-between items-center">
           <label htmlFor="gopay" className="flex items-center space-x-2">
-            <img src="/gopay-logo.png" alt="GoPay" className="w-6" />
+            <input
+              type="radio"
+              id="gopay"
+              name="paymentMethod"
+              className="mr-2"
+            />
             <span>GoPay</span>
           </label>
+          <img src="/img/logo/gopay.png" alt="GoPay" />
         </div>
-        <div className="mb-4 border p-2 rounded rounded-sm flex flex-row items-center">
-          <input type="radio" id="ovo" name="paymentMethod" className="mr-2" />
+        <div className="mb-4 border p-2 rounded rounded-sm flex justify-between items-center">
           <label htmlFor="ovo" className="flex items-center space-x-2">
-            <img src="/ovo-logo.png" alt="OVO" className="w-6" />
+            <input
+              type="radio"
+              id="ovo"
+              name="paymentMethod"
+              className="mr-2"
+            />
             <span>OVO</span>
           </label>
+          <img src="/img/logo/ovo.png" alt="OVO" />
         </div>
-        <div className="mb-4 border p-2 rounded rounded-sm flex flex-row items-center">
-          <input
-            type="radio"
-            id="linkaja"
-            name="paymentMethod"
-            className="mr-2"
-          />
+        <div className="mb-4 border p-2 rounded rounded-sm flex justify-between items-center">
           <label htmlFor="linkaja" className="flex items-center space-x-2">
-            <img src="/linkaja-logo.png" alt="LinkAja" className="w-6" />
+            <input
+              type="radio"
+              id="linkaja"
+              name="paymentMethod"
+              className="mr-2"
+            />
             <span>LinkAja</span>
           </label>
+          <img src="/img/logo/link-aja.png" alt="LinkAja" />
         </div>
-        <div className="mb-4 border p-2 rounded rounded-sm flex flex-row items-center">
-          <input
-            type="radio"
-            id="shopeepay"
-            name="paymentMethod"
-            className="mr-2"
-          />
+        <div className="mb-4 border p-2 rounded rounded-sm flex justify-between items-center">
           <label htmlFor="shopeepay" className="flex items-center space-x-2">
-            <img src="/shopeepay-logo.png" alt="ShopeePay" className="w-6" />
+            <input
+              type="radio"
+              id="shopeepay"
+              name="paymentMethod"
+              className="mr-2"
+            />
             <span>ShopeePay</span>
           </label>
+          <img src="/img/logo/shopee-pay.png" alt="ShopeePay" />
         </div>
       </div>
     </div>
--- frontend/src/components/payment/paymentMethod/Va.jsx
+++ frontend/src/components/payment/paymentMethod/Va.jsx
@@ -1,34 +1,53 @@
-import React from "react";
-
 function Va() {
   return (
     <div className="mb-4">
       <h3 className="text-lg font-semibold mb-2">Virtual Account</h3>
       <div className="space-y-2">
-        <div className="mb-4 border p-2 rounded rounded-sm flex flex-row items-center">
-          <input type="radio" id="bca" name="paymentMethod" className="mr-2" />
-          <label htmlFor="bca" className="flex items-center flex-row space-x-2">
+        <div className="mb-4 border p-2 rounded rounded-sm flex justify-between items-center">
+          <label htmlFor="bca" className="flex items-center space-x-2">
+            <input
+              type="radio"
+              id="bca"
+              name="paymentMethod"
+              className="mr-2"
+            />
             <span>BCA Virtual Account</span>
-            <img src="/bca-logo.png" alt="BCA" className="w-6" />
           </label>
+          <img src="/img/logo/bca.png" alt="BCA" />
         </div>
-        <div className="mb-4 border p-2 rounded rounded-sm flex flex-row items-center">
-          <input type="radio" id="bni" name="paymentMethod" className="mr-2" />
+        <div className="mb-4 border p-2 rounded rounded-sm flex justify-between items-center">
           <label htmlFor="bni" className="flex items-center space-x-2">
-            <img src="/bni-logo.png" alt="BNI" className="w-6" />
+            <input
+              type="radio"
+              id="bni"
+              name="paymentMethod"
+              className="mr-2"
+            />
             <span>BNI Virtual Account</span>
           </label>
+          <img src="/img/logo/bni.png" alt="BNI" />
         </div>
-        <div className="mb-4 border p-2 rounded rounded-sm flex flex-row items-center">
-          <input
-            type="radio"
-            id="mandiri"
-            name="paymentMethod"
-            className="mr-2"
-          />
+        <div className="mb-4 border p-2 rounded rounded-sm flex justify-between items-center">
           <label htmlFor="mandiri" className="flex items-center space-x-2">
-            <img src="/mandiri-logo.png" alt="Mandiri" className="w-6" />
+            <input
+              type="radio"
+              id="mandiri"
+              name="paymentMethod"
+              className="mr-2"
+            />
             <span>Mandiri Virtual Account</span>
+          </label>
+          <img src="/img/logo/mandiri.png" alt="Mandiri" />
+        </div>
+        <div className="mb-4 border p-2 rounded rounded-sm flex justify-between items-center">
+          <label htmlFor="other" className="flex items-center space-x-2">
+            <input
+              type="radio"
+              id="other"
+              name="paymentMethod"
+              className="mr-2"
+            />
+            <span>Other Bank</span>
           </label>
         </div>
       </div>
