53b3bc57cc05bb3d8fce59032b838fb4868d00c5
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-10-23 23:14:21 +0700

    add waiting payment

--- frontend/src/views/payment/paymentMethod/Aside.jsx
+++ frontend/src/views/payment/paymentMethod/Aside.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 function ViewAside(props) {
-  const { isAside, handlePayment } = props;
+  const { isPaymentMethod, isWaitingPayment, handlePayment } = props;
   return (
-    <aside className="w-1/3 p-6">
+    <aside className="p-6">
       <h3 className="text-2xl font-bold mb-4">Event Details</h3>
       <div className="mb-4">
         <img src="/event-image.png" alt="Event" className="w-full rounded-md" />
@@ -39,18 +39,19 @@ function ViewAside(props) {
         <span>Total:</span>
         <span className="text-sm text-gray-900">Rp. 762,000</span>
       </div>
-      {!!isAside ? (
-        <button className="w-full bg-blueDefault hover:bg-blueDarkest text-white py-2 mt-4 rounded-md">
-          Pay Now
-        </button>
-      ) : (
-        <button
-          onClick={handlePayment}
-          className="mt-4 w-full bg-blueDefault hover:bg-blueDarkest text-white px-4 py-2 rounded"
-        >
-          Continue to Payment
-        </button>
-      )}
+      {!isWaitingPayment &&
+        (!!isPaymentMethod ? (
+          <button className="w-full bg-blueDefault hover:bg-blueDarkest text-white py-2 mt-4 rounded-md">
+            Pay Now
+          </button>
+        ) : (
+          <button
+            onClick={handlePayment}
+            className="mt-4 w-full bg-blueDefault hover:bg-blueDarkest text-white px-4 py-2 rounded"
+          >
+            Continue to Payment
+          </button>
+        ))}
     </aside>
   );
 }
