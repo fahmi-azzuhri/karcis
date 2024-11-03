42037a38819bdd2f54fbcf02a5a9b26ebdea0711
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-11-03 21:57:44 +0700

    add signin signup api

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import { generateAdminAccount } from "./services/adminService.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/auth", authRoutes);
+
+generateAdminAccount();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
