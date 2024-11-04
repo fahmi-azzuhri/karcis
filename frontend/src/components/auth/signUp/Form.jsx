51dffe2ec11d7f88c9a68afb2c04789f8a9f275a
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-11-04 11:44:06 +0700

    atomic signup page

--- frontend/src/components/auth/signUp/Form.jsx
+++ frontend/src/components/auth/signUp/Form.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import ViewForm from "../../../views/auth/Form";
 
 function Form(props) {
   const { handleLogin } = props;
@@ -7,6 +8,7 @@ function Form(props) {
   const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const isValid = firstname && lastname && email && password;
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
@@ -25,75 +27,19 @@ function Form(props) {
     }
   };
   return (
-    <form className="space-y-4" onSubmit={handleSignup}>
-      <div className="flex space-x-4">
-        <div className="w-1/2">
-          <label className="block text-gray-700">First Name</label>
-          <input
-            type="text"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Your name"
-            value={firstname}
-            onChange={(e) => setFirstname(e.target.value)}
-          />
-        </div>
-        <div className="w-1/2">
-          <label className="block text-gray-700">Last Name</label>
-          <input
-            type="text"
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Your name"
-            value={lastname}
-            onChange={(e) => setLastname(e.target.value)}
-          />
-        </div>
-      </div>
-      <div>
-        <label className="block text-gray-700">Email address</label>
-        <input
-          type="email"
-          className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder="name@example.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label className="block text-gray-700">Password</label>
-        <input
-          type="password"
-          className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder="Your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-
-      <p className="text-sm text-gray-500 text-right">
-        Already have an account?
-        <span
-          onClick={handleLogin}
-          className=" px-1 text-blueDefault cursor-pointer"
-        >
-          Sign in
-        </span>
-        now!
-      </p>
-
-      <div className="p-4 bg-[#F0EFFE]">
-        <p className="text-sm text-blueDefault">
-          E-tickets will be sent to your email address, please make sure your
-          email address is correct.
-        </p>
-      </div>
-
-      <button
-        type="submit"
-        className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:bg-blue-200"
-      >
-        Sign up
-      </button>
-    </form>
+    <ViewForm
+      firstname={firstname}
+      lastname={lastname}
+      email={email}
+      password={password}
+      setFirstname={setFirstname}
+      setLastname={setLastname}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      handleSignup={handleSignup}
+      isValid={isValid}
+      handleLogin={handleLogin}
+    />
   );
 }
 
