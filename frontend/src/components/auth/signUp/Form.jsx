e20fd132bd6c7d49e26df54bc34a4fc8cbd6ef38
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-11-05 12:58:35 +0700

    fix atomic

--- frontend/src/components/auth/signUp/Form.jsx
+++ frontend/src/components/auth/signUp/Form.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import ViewForm from "../../../views/auth/Form";
+import ViewForm from "../../../views/auth/signUp/Form";
 
 function Form(props) {
   const { handleLogin } = props;
