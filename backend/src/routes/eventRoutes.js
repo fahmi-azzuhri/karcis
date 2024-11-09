550a7d1fec2138ede73919daa770f47e53765550
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-11-09 18:16:27 +0700

    add method get all event and get by id

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -3,6 +3,7 @@ import upload from "../config/multer.js";
 import prisma from "../config/prisma.js";
 const router = express.Router();
 
+//POST: Create a new event
 router.post("/events", upload.single("image"), async (req, res) => {
   try {
     const {
@@ -48,6 +49,34 @@ router.post("/events", upload.single("image"), async (req, res) => {
     });
 
     res.status(201).json(newEvent);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// GET: Retrieve all events
+router.get("/events", async (req, res) => {
+  try {
+    const events = await prisma.event.findMany();
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// GET: Retrieve a specific event by ID
+router.get("/events/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const event = await prisma.event.findUnique({
+      where: { id: parseInt(id, 10) },
+    });
+
+    if (!event) {
+      return res.status(404).json({ error: "Event not found" });
+    }
+
+    res.status(200).json(event);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
