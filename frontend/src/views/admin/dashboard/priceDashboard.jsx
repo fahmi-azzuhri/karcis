14600fc32958aaeb78bb28788477e26009fdb319
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-11-12 21:24:12 +0700

    integration price dashboard

---
+++ frontend/src/views/admin/dashboard/priceDashboard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function ViewPriceDashboard(props) {
+  const { events } = props;
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <main className="flex-1 p-4 overflow-hidden">
+        <div className="bg-white rounded-md shadow-md overflow-x-auto max-w-full">
+          <table className="min-w-full text-sm sm:text-base">
+            <thead className="bg-blue-500 text-white">
+              <tr>
+                <th className="p-2 sm:p-3 text-left text-sm sm:text-base">
+                  Vip Price
+                </th>
+                <th className="p-2 sm:p-3 text-left text-sm sm:text-base">
+                  Vvip Price
+                </th>
+                <th className="p-2 sm:p-3 text-left text-sm sm:text-base">
+                  Ngedate Price
+                </th>
+                <th className="p-2 sm:p-3 text-left text-sm sm:text-base">
+                  Ngedate Premium Price
+                </th>
+                <th className="p-2 sm:p-3 text-left text-sm sm:text-base">
+                  Rame Price
+                </th>
+                <th className="p-2 sm:p-3 text-left text-sm sm:text-base">
+                  Rame Premium Price
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {events.map((event) => (
+                <tr key={event.id} className="border-b hover:bg-gray-100">
+                  <td className="p-2 sm:p-3">Rp {event.vipPrice}</td>
+                  <td className="p-2 sm:p-3">Rp {event.vvipPrice}</td>
+                  <td className="p-2 sm:p-3">Rp {event.ngedatePrice}</td>
+                  <td className="p-2 sm:p-3">Rp {event.ngedatePremiumPrice}</td>
+                  <td className="p-2 sm:p-3">Rp {event.ramePrice}</td>
+                  <td className="p-2 sm:p-3">Rp {event.ramePremiumPrice}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default ViewPriceDashboard;
