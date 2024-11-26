9a4e9d9b3df21aa15c6359e39f378f2bf2684e05
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-11-26 14:09:42 +0700

    integration upcoming event

--- frontend/src/views/home/event/upcomingEvent/index.jsx
+++ frontend/src/views/home/event/upcomingEvent/index.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 function ViewUpcomingEvent(props) {
-  const { settings, cards, Slider } = props;
+  const { settings, events, Slider } = props;
   return (
     <div className="max-w-screen-2xl mx-auto p-8">
       <Slider {...settings}>
-        {cards.map((card, index) => (
+        {events.map((event, index) => (
           <div key={index} className="px-4">
             <div className="bg-white shadow-lg rounded-lg overflow-hidden">
               <img
-                src={card.image}
-                alt={card.text}
+                src={`${import.meta.env.VITE_API_ENDPOINT}${event.imageUrl}`}
+                alt={event.title}
                 className="w-full h-48 object-cover"
               />
               <div className="p-4">
-                <h3 className="text-lg font-bold">{card.text}</h3>
+                <h3 className="text-lg font-bold">{event.title}</h3>
               </div>
             </div>
           </div>
