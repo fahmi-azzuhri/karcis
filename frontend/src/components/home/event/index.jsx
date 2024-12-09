904e75f6815461a5dced7bfe843ec11cf6681f4c
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-12-09 11:43:56 +0700

    delete top selling component

--- frontend/src/components/home/event/index.jsx
+++ frontend/src/components/home/event/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import UpcomingEvent from "./upcomingEvent";
-import TopSelling from "./topSelling";
 import BrowseArts from "./browseArts";
 
 function Events() {
@@ -9,10 +8,6 @@ function Events() {
       <div className="px-12">
         <h3 className="text-2xl font-bold text-blackDefault">Upcoming Event</h3>
         <UpcomingEvent />
-      </div>
-      <div className="px-12">
-        <h3 className="text-2xl font-bold text-blackDefault">Top Selling</h3>
-        <TopSelling />
       </div>
       <div className="px-12">
         <h3 className="text-2xl font-bold text-blackDefault">Browse Arts</h3>
