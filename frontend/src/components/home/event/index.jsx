8f2095bf005b1917c21d42e835f29ffffd5bd080
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-12-09 11:51:42 +0700

    add browse concert components

--- frontend/src/components/home/event/index.jsx
+++ frontend/src/components/home/event/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UpcomingEvent from "./upcomingEvent";
 import BrowseArts from "./browseArts";
+import BrowseConcerts from "./browseConcerts";
 
 function Events() {
   return (
@@ -12,6 +13,12 @@ function Events() {
       <div className="px-12">
         <h3 className="text-2xl font-bold text-blackDefault">Browse Arts</h3>
         <BrowseArts />
+      </div>
+      <div className="px-12">
+        <h3 className="text-2xl font-bold text-blackDefault">
+          Browse Concerts
+        </h3>
+        <BrowseConcerts />
       </div>
     </>
   );
