cfcd9d69902068750c1319bdc5dac34197e2973f
Fahmi Azzuhri Efki <fahmiazzuhriefki@gmail.com>
2024-12-18 21:01:07 +0700

    add art model

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import concertRoutes from "./routes/concertRoutes.js";
+import artRoutes from "./routes/artRoutes.js";
 import { generateAdminAccount } from "./services/adminService.js";
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/api", eventRoutes);
 app.use("/api", concertRoutes);
+app.use("/api", artRoutes);
 app.use("/uploads", express.static("uploads"));
 
 // Generate an admin account on server start
