35c355e23d2e67866212409d65584af9ac7adad5
Fahmi Azzuhri Efki <fahmiazzuhriefki@gmail.com>
2024-12-26 21:03:40 +0700

    add dashboard art method GET

--- frontend/src/components/admin/dashboard/artDashboard.jsx
+++ frontend/src/components/admin/dashboard/artDashboard.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import ViewArtDashboard from "../../../views/admin/dashboard/artDashboard";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 function ArtDashboard() {
   const [isOpen, setIsOpen] = useState(false);
   const handleCloseModal = () => {
@@ -11,9 +14,30 @@ function ArtDashboard() {
   const handleArtAdded = () => {
     setIsOpen(false);
   };
+
+  const {
+    data: arts,
+    isPending,
+    isFetching,
+    error,
+  } = useQuery({
+    queryKey: ["arts"],
+    queryFn: async () =>
+      await axios
+        .get(`${import.meta.env.VITE_API_ENDPOINT}/api/arts`)
+        .then((response) => response.data),
+  });
+
+  if (isPending || isFetching) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error fetching arts : {error.message}</div>;
+  }
   return (
     <div>
       <ViewArtDashboard
+        arts={arts}
         isOpen={isOpen}
         handleCloseModal={handleCloseModal}
         handleOpenModal={handleOpenModal}
